4b6a106b203c140cf8c593aed8de0af5d567d147
Jesus-1815 <jesus.pimentel@utp.ac.pa>
2026-03-08 16:20:38 -0500

    Subiendo requerimientos y cambios en el backend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import smtplib
 from email.mime.text import MIMEText
 import os
@@ -7,34 +8,43 @@ EMAIL_USER = os.getenv("EMAIL_USER")
 EMAIL_PASS = os.getenv("EMAIL_PASS")
 
 app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return "Backend funcionando"
 
 @app.route("/send-email", methods=["POST"])
 def send_email():
 
-    data = request.json
+    try:
+        data = request.json
 
-    name = data["name"]
-    email = data["email"]
-    subject = data["subject"]
-    message = data["message"]
+        name = data["name"]
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
 
-    body = f"""
-    Nombre: {name}
-    Email: {email}
+        body = f"""
+Nombre: {name}
+Email: {email}
 
-    Mensaje:
-    {message}
-    """
+Mensaje:
+{message}
+"""
 
-    msg = MIMEText(body)
-    msg["Subject"] = subject
-    msg["From"] = EMAIL_USER
-    msg["To"] = EMAIL_USER
+        msg = MIMEText(body)
+        msg["Subject"] = subject
+        msg["From"] = EMAIL_USER
+        msg["To"] = EMAIL_USER
 
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(EMAIL_USER, EMAIL_PASS)
-    server.sendmail(EMAIL_USER, EMAIL_USER, msg.as_string())
-    server.quit()
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(EMAIL_USER, EMAIL_PASS)
+        server.sendmail(EMAIL_USER, EMAIL_USER, msg.as_string())
+        server.quit()
 
-    return jsonify({"success": True})
+        return jsonify({"success": True})
+
+    except Exception as e:
+        return jsonify({"success": False, "error": str(e)})
